f23426e51d6ca4c8f6af5f6001b4325b4d9534ca
Anselmo-cpu <anselmoui16@gmail.com>
2025-11-24 22:24:27 -0300

    props

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,11 @@ function App() {
       </section>
     <hr />
       <div>
-        <Item title="M vave MK-300" price="$250,000" img="https://acdn-us.mitiendanube.com/stores/005/685/596/products/audioimport-2025-10-01t125854-539-70c4654f14bc7a74db17593341128205-640-0.webp" description="Pedalera Multiefecto de M Vave"/>
+        <Item title="M vave MK-300" price="$250,000" categories="Digital"  handleClick = {() => alert("M vave MK-300")} img="https://acdn-us.mitiendanube.com/stores/005/685/596/products/audioimport-2025-10-01t125854-539-70c4654f14bc7a74db17593341128205-640-0.webp" description="Pedalera Multiefecto de M Vave"/>
 
-        <Item title="M vave Tank G" price="$150,000" img="https://www.musicarms.net/wp-content/uploads/2024/04/M-VAVE-Tank-G-%E0%B9%80%E0%B8%AD%E0%B8%9F%E0%B9%80%E0%B8%9F%E0%B8%84.jpg" description="Pedalera Multiefecto de M Vave"/> 
+        <Item title="M vave Tank G" price="$150,000" categories="Digital"  handleClick = {() => alert("M vave Tank G")} img="https://www.musicarms.net/wp-content/uploads/2024/04/M-VAVE-Tank-G-%E0%B9%80%E0%B8%AD%E0%B8%9F%E0%B9%80%E0%B8%9F%E0%B8%84.jpg" description="Pedalera Multiefecto de M Vave"/> 
 
-        <Item title="M Vave Cube Baby" price ="$100,000" img="https://http2.mlstatic.com/D_NQ_NP_677756-MLU73129178458_122023-O.webp" description="Pedalera Multiefecto de M Vave"/>
+        <Item title="M Vave Cube Baby" price ="$100,000" categories="Digital"  handleClick = {() => alert("M Vave Cube Baby")} img="https://http2.mlstatic.com/D_NQ_NP_677756-MLU73129178458_122023-O.webp" description="Pedalera Multiefecto de M Vave"/>
       </div>
     </>
   )
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,15 +1,18 @@
 function Item(props) {
-    console.log(props);
+    const { title, img, price, description, handleClick } = props
+
  return (
     <>
-        <h3>{props.title}</h3>
-        <img  width="200" src={props.img} alt={props.title} />
-        <p>{props.price}</p>
-        <p>{props.description}</p>
-        <button>Ver Producto</button>
+        <h3>{title}</h3>
+        <img  width="200" src={img} alt={title} />
+        <p>{price}</p>
+        <p>{description}</p>
+        <button onClick={handleClick}>Ver Producto</button>
     </>
  )
 }
+
+
 
 
 export default Item;
